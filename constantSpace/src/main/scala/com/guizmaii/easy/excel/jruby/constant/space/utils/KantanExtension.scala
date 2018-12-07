617cf32175ca9e136f6fc8f07bf3f1d70eeb39b1
jules Ivanic <jules.ivanic@gmail.com>
2018-12-07 11:44:58 +0100

    Improve performances

--- constantSpace/src/main/scala/com/guizmaii/easy/excel/jruby/constant/space/utils/KantanExtension.scala
+++ constantSpace/src/main/scala/com/guizmaii/easy/excel/jruby/constant/space/utils/KantanExtension.scala
@@ -2,6 +2,7 @@ package com.guizmaii.easy.excel.jruby.constant.space.utils
 
 import kantan.csv.{CellDecoder, CellEncoder, RowDecoder, RowEncoder}
 
+import scala.collection.mutable.ArrayBuffer
 import scala.reflect.ClassTag
 
 private[space] object KantanExtension {
@@ -15,9 +16,9 @@ private[space] object KantanExtension {
       implicit CellDecoder: CellDecoder[A]
   ): RowDecoder[Array[A]] =
     RowDecoder.fromUnsafe { array =>
-      val acc = Array.empty[A]
-      for (a <- array) acc :+ CellDecoder.unsafeDecode(a)
-      acc
+      val acc = ArrayBuffer.empty[A]
+      for (a <- array) acc += CellDecoder.unsafeDecode(a)
+      acc.toArray
     }
 
   implicit final def arrayEncoder[A](implicit CellEncoder: CellEncoder[A]): RowEncoder[Array[A]] =
