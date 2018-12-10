88e645b95135e39b2ab34806a245266e2979f152
jules Ivanic <jules.ivanic@gmail.com>
2018-12-10 15:42:26 +0100

    Init tests

---
+++ constantSpace/src/test/scala/com/guizmaii/easy/excel/jruby/constant/space/EasyExcelJRubySpec.scala
@@ -0,0 +1,10 @@
+package com.guizmaii.easy.excel.jruby.constant.space
+import org.scalatest.{FlatSpec, Matchers}
+
+class EasyExcelJRubySpec extends FlatSpec with Matchers {
+
+  "true" should "be true" in {
+    true shouldBe true
+  }
+
+}
